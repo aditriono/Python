99074838d5aa01deda488737d2a42a7f2f6282ba
aditriono <82079666+aditriono@users.noreply.github.com>
2021-04-07 13:39:52 +0700

    tki07

---
+++ tki07.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(3):
+    #window.columnconfigure(i, weight=1, minisize=75)
+    #window.rowconfigure(i, weight=1, minisize=50)
+    
+	for j in range(0, 3):
+		frame = tk.Frame(
+			master=window, 
+            relief=tk.GROOVE, 
+            borderwidth=3
+		)
+		frame.grid(row=i, column=j, padx=3, pady=3)
+        
+		label = tk.Label(master=frame, text=f"Row {i}\ncolumn {j}")
+		label.pack(padx=3, pady=3)
+			
+window.mainloop()
+
